69a50387b8829aaba5458fd1bf40d29d4748cb93
Jianshan Jiang <jiangjianshan1103@gmail.com>
2025-04-12 14:27:34 +0800

    Increased to more than 269 libraries

---
+++ packages/graphene/build.bat
@@ -0,0 +1,106 @@
+@echo off
+setlocal enabledelayedexpansion
+rem
+rem  Build script for the current library, it should not be called directly from the
+rem  command line, but should be called from mpt.py.
+rem
+rem  The values of these environment variables come from mpt.py:
+rem  ARCH            - x64 or x86
+rem  ROOT_DIR        - root location of msvc-pkg
+rem  PREFIX          - install location of current library
+rem  PREFIX_PATH     - install location of third party libraries
+rem  _PREFIX         - default install location if not list in settings.yaml
+rem
+rem  Copyright (c) 2024 Jianshan Jiang
+rem
+rem  Permission is hereby granted, free of charge, to any person obtaining a copy
+rem  of this software and associated documentation files (the "Software"), to deal
+rem  in the Software without restriction, including without limitation the rights
+rem  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+rem  copies of the Software, and to permit persons to whom the Software is
+rem  furnished to do so, subject to the following conditions:
+rem
+rem  The above copyright notice and this permission notice shall be included in all
+rem  copies or substantial portions of the Software.
+rem
+rem  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+rem  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+rem  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+rem  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+rem  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+rem  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+rem  SOFTWARE.
+
+for /f "delims=" %%i in ('yq -r ".name" config.yaml') do set PKG_NAME=%%i
+for /f "delims=" %%i in ('yq -r ".version" config.yaml') do set PKG_VER=%%i
+if "%ROOT_DIR%"=="" (
+    echo Don't directly run %~nx0 from command line.
+    echo To build !PKG_NAME! and its dependencies, please go to the root location of msvc-pkg, and then press
+    echo mpt !PKG_NAME!
+    goto :end
+)
+call "%ROOT_DIR%\compiler.bat" %ARCH%
+set RELS_DIR=%ROOT_DIR%\releases
+set SRC_DIR=%RELS_DIR%\%PKG_NAME%-%PKG_VER%
+set BUILD_DIR=%SRC_DIR%\build%ARCH:x=%
+set C_OPTS=-nologo -MD -diagnostics:column -wd4819 -wd4996 -fp:precise -openmp:llvm -Zc:__cplusplus -experimental:c11atomics
+set C_DEFS=-DWIN32 -D_WIN32_WINNT=_WIN32_WINNT_WIN10 -D_CRT_DECLARE_NONSTDC_NAMES -D_CRT_SECURE_NO_DEPRECATE -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_DEPRECATE -D_CRT_NONSTDC_NO_WARNINGS -D_USE_MATH_DEFINES -DNOMINMAX
+
+call :configure_stage
+call :build_stage
+call :install_package
+goto :end
+
+rem ==============================================================================
+rem  Configure package and ready to build
+rem ==============================================================================
+:configure_stage
+call :clean_build
+echo "Configuring %PKG_NAME% %PKG_VER%"
+mkdir "%BUILD_DIR%"
+cd "%SRC_DIR%"
+meson setup "%BUILD_DIR%"                                                      ^
+  --buildtype=release                                                          ^
+  --prefix="%PREFIX%"                                                          ^
+  --mandir="%PREFIX%\share\man"                                                ^
+  -Dc_std=c17                                                                  ^
+  -Dc_args="%C_OPTS% %C_DEFS%"                                                 ^
+  -Dcpp_std=c++17                                                              ^
+  -Dcpp_args="-EHsc %C_OPTS% %C_DEFS%"                                         ^
+  -Ddefault_both_libraries=shared                                              ^
+  -Ddefault_library=shared                                                     ^
+  -Dgcc_vector=false                                                           ^
+  -Darm_neon=false                                                             ^
+  -Dinstalled_tests=false                                                      ^
+  -Dtests=false || exit 1
+exit /b 0
+
+rem ==============================================================================
+rem  Build package
+rem ==============================================================================
+:build_stage
+echo "Building %PKG_NAME% %PKG_VER%"
+cd "%BUILD_DIR%" && ninja -j%NUMBER_OF_PROCESSORS%
+exit /b 0
+
+rem ==============================================================================
+rem  Install package
+rem ==============================================================================
+:install_package
+echo "Installing %PKG_NAME% %PKG_VER%"
+cd "%BUILD_DIR%" && ninja install || exit 1
+for %%f in ("%PREFIX%\lib\pkgconfig\graphene*.pc") do (
+  sed -E "s#([A-Za-z]):[\\/]#/\L\1/#gI" -i "%%~f"
+)
+call :clean_build
+exit /b 0
+
+rem ==============================================================================
+rem  Clean files generated during build procedure
+rem ==============================================================================
+:clean_build
+echo "Cleaning %PKG_NAME% %PKG_VER%"
+cd "%SRC_DIR%" && if exist "%BUILD_DIR%" rmdir /s /q "%BUILD_DIR%"
+exit /b 0
+
+:end
